d9c06d60d23ce60e6bcc001504e0e4ea8ea9b67c
GBAC22 <gbac2208@gmail.com>
2024-09-06 13:57:59 -0400

    encontrar caminos
    encuentra caminos

--- obj/Release/net8.0/Dijktramvc.AssemblyInfo.cs
+++ obj/Release/net8.0/Dijktramvc.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dijktramvc")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+750c94efadb15efe610a95cedb72c3d86d358198")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dijktramvc")]
